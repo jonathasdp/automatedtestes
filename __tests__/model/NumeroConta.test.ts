db5086bebb1f7c020690f2f2bbb8ebaf9d003733
jonathasdp <jonathas.dp@gmail.com>
2023-09-22 20:32:00 -0300

    Número de conta inválido.

--- __tests__/model/NumeroConta.test.ts
+++ __tests__/model/NumeroConta.test.ts
@@ -3,7 +3,18 @@ import { NumeroConta } from "../../src/model/NumeroConta";
 describe("Número Conta", () => {
     test("Conta possui 6 dígitos.", async () => {
         const numeroConta = new NumeroConta("123456");
-        expect(numeroConta.numero).toBe("123456");
         expect(numeroConta.numero.length).toBe(6);
+    })
+
+    test("Número possui menos de 6 dígitos.", async () => {
+        expect(() => { new NumeroConta("12345") }).toThrow("Número de conta inválido.");
+    })
+
+    test("Número possui mais de 6 dígitos.", async () => {
+        expect(() => { new NumeroConta("1234567") }).toThrow("Número de conta inválido.");
+    })
+
+    test("Número possui letras.", async () => {
+        expect(() => { new NumeroConta("abc123") }).toThrow("Número de conta inválido.");
     })
 });
--- src/model/NumeroConta.ts
+++ src/model/NumeroConta.ts
@@ -1,4 +1,17 @@
 export class NumeroConta {
     constructor(public numero: string) {
+        this.validar(numero);
+    }
+
+    validar(numero: string): void {
+        if (!this.PossuiSeisDigitosNumericos(numero)) {
+            throw new Error("Número de conta inválido.");
+        }
+    }
+    
+    PossuiSeisDigitosNumericos(numero: string): boolean {
+        const rgx: RegExp = /^\d{6}$/;
+        return rgx.test(numero)
     }
 }
+
