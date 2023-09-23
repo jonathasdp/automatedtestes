c5e54ef4453e0c6ef8f281ba6983a739e272c260
jonathasdp <jonathas.dp@gmail.com>
2023-09-22 22:34:58 -0300

    Transferência com sucesso.

--- __tests__/application/TransferenciaService.test.ts
+++ __tests__/application/TransferenciaService.test.ts
@@ -3,22 +3,23 @@ import { TransferenciaDTO } from "../../src/application/dto/TransferenciaDTO";
 import { Conta } from "../../src/model/Conta";
 import { MemoriaContaRepositorio } from "../fake/MemoriaContaRepositorio";
 
-describe("Transferência serviço", () => {
-    test("Transferir valores com sucesso.", async () => {
+describe("Transferência serviço", () =>{
+
+    test("Transferência com sucesso.", () => {
         const repositorio = new MemoriaContaRepositorio();
-        const contaOrigem = new Conta("123456", 1000.0);
-        const contaDestino = new Conta("654321", 1000.0);
+        const contaOrigem = new Conta("123456", 5000);
+        const contaDestino = new Conta("654321", 5000);
         repositorio.adicionar(contaOrigem);
         repositorio.adicionar(contaDestino);
 
         const transferenciaServico = new TransferenciaServico(repositorio);
 
-        const dto = new TransferenciaDTO("123465", "654321", 100);
+        const dto = new TransferenciaDTO("123456", "654321", 100.0);
 
-        transferenciaServico.transferir(dto);
+        const recibo = transferenciaServico.transferir(dto);
 
-        expect(repositorio.buscar("123465")!.saldo).toBe(4900.00)
-        expect(repositorio.buscar("654321")!.saldo).toBe(5100.00)
-
-    })
-})
+        expect(repositorio.buscar("123456")!.saldo).toBe(4900.0);
+        expect(repositorio.buscar("654321")!.saldo).toBe(5100.0);
+        expect(recibo.length).toBe(6);
+    });
+});
