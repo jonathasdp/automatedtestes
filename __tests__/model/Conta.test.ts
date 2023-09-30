8d0d770b75134464eb52dacaa6cfca2b24e37637
jonathasdp <jonathas.dp@gmail.com>
2023-09-29 22:19:02 -0300

    Corrigindo nossos testes

--- __tests__/model/Conta.test.ts
+++ __tests__/model/Conta.test.ts
@@ -5,50 +5,36 @@ describe("Conta", () => {
         const conta = criarConta();
         conta.sacar(200.0);
         expect(conta.saldo).toBe(4800.0);
-    })
-})
-
-describe("Conta", () => {
+    });
     test("Não sacou com valor zerado.", async () => {
         const conta = criarConta();
         expect(() => conta.sacar(0)).toThrow("Valor não pode ser igual ou menor que zero.");
-    })
-})
-
-describe("Conta", () => {
+    });
     test("Não sacou com valor negativo.", async () => {
         const conta = criarConta();
         expect(() => conta.sacar(-5.0)).toThrow("Valor não pode ser igual ou menor que zero.");
-    })
-})
-
-describe("Conta", () => {
+    });
     test("Depositou corretamente.", async () => {
         const conta = criarConta();
         conta.depositar(200.0);
         expect(conta.saldo).toBe(5200.0);
-    })
-})
-
-describe("Conta", () => {
+    });
     test("Não depositou com valor zerado.", async () => {
         const conta = criarConta();
         expect(() => conta.depositar(0)).toThrow("Valor não pode ser igual ou menor que zero.");
-    })
-})
-
-describe("Conta", () => {
+    });
     test("Não depositou com valor negativo.", async () => {
         const conta = criarConta();
         expect(() => conta.sacar(-5.0)).toThrow("Valor não pode ser igual ou menor que zero.");
-    })
-})
-
-
-describe("Conta", () => {
+    });
     test("sacar valor acima do saldo", async () => {
         const conta = criarConta("123456", 199.0);
         expect(() => { conta.sacar(200.0); }).toThrow("Saques devem possuir saldo.");
+    });
+    test("sacar todo o saldo", async () => {
+        const conta = criarConta();
+        conta.sacar(5000.0);
+        expect(conta.saldo).toBe(0);
     });
 })
 
--- __tests__/model/Recibo.test.ts
+++ __tests__/model/Recibo.test.ts
@@ -4,5 +4,10 @@ describe("Recibo", () => {
     test("Criou recibo.", async () => {
         const recibo = new Recibo();
         expect(recibo.codigo.length).toBe(6);
-    })
+    });
+    test("criar recibos diferentes", async () => {
+        const recibo1 = new Recibo();
+        const recibo2 = new Recibo();
+        expect(recibo1.codigo).not.toBe(recibo2.codigo);
+    });
 })
